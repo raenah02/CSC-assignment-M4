b9c7bbeaac0bb424cac69187c238a0e6d5e0cb7e
raenah02 <raenah.solat@gmail.com>
2025-11-22 17:58:29 -0500

    Task 1.1: Fixed CheckStyle issues

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,41 @@
 package theater;
 
+/**
+ * Class representing a play with a name and type.
+ */
 public class Play {
 
-    public String name;
-    public String type;
+    // Encapsulated fields
+    private final String name;
+    private final String type;
 
+    /**
+     * Creates a new Play with the given name and type.
+     *
+     * @param name the name of the play
+     * @param type the type of the play (e.g., tragedy, comedy)
+     */
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
     }
+
+    /**
+     * Returns the name of the play.
+     *
+     * @return the play's name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Returns the type of the play.
+     *
+     * @return the play's type
+     */
+    public String getType() {
+        return type;
+    }
 }
+
